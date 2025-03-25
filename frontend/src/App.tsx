373157ccb0ea0a52cb075ec8508584d4ceb7546f
Ramez Chreide <ramiz_chreide@outlook.com>
2025-03-25 12:41:46 +0100

    fix: Routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,26 +2,14 @@ import {useEffect, useState} from 'react'
 import axios from 'axios';
 import {AppUser} from "./types.ts";
 import {WelcomePage} from "./pages/WelcomePage.tsx";
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, useNavigate} from "react-router-dom";
 import ProtectedRoutes from "./shared/ProtectedRoutes.tsx";
 import { Navbar } from "./shared/Navbar.tsx";
 import {Customers} from "./pages/Customers.tsx";
 
 function App() {
   const [appUser, setAppUser] = useState<AppUser | undefined | null>(undefined);
-
-  function login() {
-    // window.location.host is whats after www. and the port
-    // e.g. localhost:5173
-    // window.location.origin is complete url with port
-    // e.g. http://localhost:5173
-    // if url is same as localhost then we are in production
-    // then our backend is at localhost:8080
-    // if url isnt same as localhost then we are in development
-    // then our backend is at url:8080
-    const host = window.location.host === 'localhost:5173' ? 'http://localhost:8080' : window.location.origin
-    window.open(host + '/oauth2/authorization/github', '_self')
-  }
+  const navigate = useNavigate();
 
   function getMe() {
     axios.get("/api/auth/me")
@@ -32,16 +20,26 @@ function App() {
         })
   }
 
-  useEffect(getMe, []);
+  useEffect(() => {
+    getMe();
+  }, []);
 
+  useEffect(() => {
+    if (appUser) {
+      navigate("/customers");
+    }
+  }, [appUser, navigate]);
 
-    return (
-    <Routes>
-      <Route path="/" element={appUser ? <Navbar/> : <WelcomePage onGoogleLogin={login} onGitHubLogin={login}/>} />
-      <Route element={<ProtectedRoutes appUser={appUser} />}>
-        <Route path="/customers" element={<Customers/>} />
-      </Route>
-    </Routes>
+  return (
+      <>
+        {appUser && <Navbar/>}
+        <Routes>
+          <Route path="/" element={<WelcomePage/>} />
+          <Route element={<ProtectedRoutes appUser={appUser} />}>
+            <Route path="/customers" element={<Customers/>} />
+          </Route>
+        </Routes>
+      </>
   )
 }
 
