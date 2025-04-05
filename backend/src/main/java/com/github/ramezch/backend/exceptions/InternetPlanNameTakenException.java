539bc057f449f1d1911499a2a7aab4dcb3ea6872
Ramez Chreide <ramiz_chreide@outlook.com>
2025-04-05 18:31:20 +0200

    add: integration tests internet plan

--- backend/src/main/java/com/github/ramezch/backend/exceptions/InternetPlanNameTakenException.java
+++ backend/src/main/java/com/github/ramezch/backend/exceptions/InternetPlanNameTakenException.java
@@ -2,6 +2,6 @@ package com.github.ramezch.backend.exceptions;
 
 public class InternetPlanNameTakenException extends RuntimeException {
     public InternetPlanNameTakenException(String message) {
-        super(message);
+        super("Internet plan name '" + message + "' is already taken.");
     }
 }
--- backend/src/main/java/com/github/ramezch/backend/exceptions/InternetPlanNotFoundException.java
+++ backend/src/main/java/com/github/ramezch/backend/exceptions/InternetPlanNotFoundException.java
@@ -2,6 +2,6 @@ package com.github.ramezch.backend.exceptions;
 
 public class InternetPlanNotFoundException extends RuntimeException {
   public InternetPlanNotFoundException(String message) {
-    super(message);
+    super("Internet plan with id: '" + message + "' not found.");
   }
 }
